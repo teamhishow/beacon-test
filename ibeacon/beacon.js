1327d1f35eea4ca2920a277717be187ef3d980e8
Yu Nakata <y-nakata@outlook.com>
2019-12-14 12:00:11 +0900

    uuidを小文字に変更

--- ibeacon/beacon.js
+++ ibeacon/beacon.js
@@ -1,6 +1,6 @@
 Bleacon = require('bleacon');
-//const uuid = 'baaaaaaabbbbccccddddeeeeeeeeeeee';
-const uuid = 'AAAAAAAABBBBCCCCDDDDEEEEEEEEEEEE';
+const uuid = 'aaaaaaaabbbbccccddddeeeeeeeeeeee';
+//const uuid = 'AAAAAAAABBBBCCCCDDDDEEEEEEEEEEEE';
 //const uuid = '222B4D44-A86A-4A44-B270-AB04311924F6';
 const major = 12;
 const minor = 34;
